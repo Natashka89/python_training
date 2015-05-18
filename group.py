49d3e6bedebd514f28b4e2e4a6cbbd8beaf612b2
Natashka89 <natusikbol@gmail.com>
2015-05-18 21:25:56 +0300

    Добавление группы. Выделение класса.

---
+++ group.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Администратор'
+
+class Group:
+
+    def __init__(self, name, header, footer):
+        self.name = name
+        self.header = header
+        self.footer = footer
