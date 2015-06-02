65cab0509b0d32152033b68ca2e6a9d310c8e4fe
Natashka89 <natusikbol@gmail.com>
2015-06-02 22:34:45 +0300

    Add open_home_page()

--- fixture/application.py
+++ fixture/application.py
@@ -9,17 +9,10 @@ from fixture.contact import ContactHelper
 class Application:
     def __init__(self):
         self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
+        self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
-
-    def is_valid(self):
-        try:
-            self.wd.current_url
-            return True
-        except:
-            return False
 
     def open_home_page(self):
         wd = self.wd
@@ -28,6 +21,13 @@ class Application:
     def return_to_home_page(self):
         wd = self.wd
         wd.find_element_by_link_text("home page").click()
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
 
     def destroy(self):
         self.wd.quit()
