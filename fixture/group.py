7078955d22d344aafedef5154d9616cbe238db78
Natashka89 <natusikbol@gmail.com>
2015-06-06 11:54:26 +0300

    Add optimization on page moving in group tests

--- fixture/group.py
+++ fixture/group.py
@@ -7,7 +7,8 @@ class GroupHelper(Manager):
 
     def return_to_groups_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("group page").click()
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new"))>0):
+            wd.find_element_by_link_text("group page").click()
 
     def create(self, group):
         wd = self.app.wd
@@ -58,7 +59,8 @@ class GroupHelper(Manager):
 
     def open_groups_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new"))>0):
+            wd.find_element_by_link_text("groups").click()
 
     def count(self):
         wd = self.app.wd
